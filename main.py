732f61da2ebb23ff7e4a156d6f261a463e4dd316
Moojig Battsogt <me@moojig.dev>
2022-05-11 23:28:30 -0400

    idk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 from server import app
 
-logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s', level=logging.DEBUG,
-                    # filename='data/minecraft_automation.log', encoding='utf-8'
-                    )
+logging.basicConfig(
+    format='[%(asctime)s] %(levelname)s: %(message)s', level=logging.DEBUG)
+app
